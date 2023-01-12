5265f00b6a5ca1a0ae89f1f64dfb4b7d685dc3f1
Raiford Brookshire <formulacoder@outlook.com>
2023-01-11 23:59:48 -0600

    Added initial Project and minimal packages

---
+++ Dna.Tool/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using CliWrap;
+using CliWrap.Buffered;
+using CliWrap.EventStream;
+using System.CommandLine;
+using System.Diagnostics;
+
+namespace Dna.Tool
+{
+	internal class Program
+	{
+		async static Task Main(string[] args)
+		{
+
+			//RootCommand root = new RootCommand("dna")
+			//{
+				
+
+			//}
+			
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
